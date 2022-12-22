8e059304cbea05398fcc0ee448de3dec5966183a
alexander.gorin@umbrellait.com <alexander.gorin@umbrellait.com>
2022-12-22 18:40:02 +0600

    yml changed

--- src/components/navbar/NavItems.tsx
+++ src/components/navbar/NavItems.tsx
@@ -6,41 +6,41 @@ import { SCREEN } from '../../containers/responsive';
 import menuStyles from './menuStyles';
 
 const NavItems = () => {
-    const isMobile = useMediaQuery({maxWidth: SCREEN.sm});
-    if (isMobile) 
+  const isMobile = useMediaQuery({maxWidth: SCREEN.sm});
+  if (isMobile) 
     return (
-        <Menu right styles={menuStyles}>
-            <ListContainer>
-                <NavItem menu>
-                    <a href='#'>Home</a>
-                </NavItem>
-                <NavItem menu>
-                    <a href='/cars'>Cars</a>
-                </NavItem>
-                <NavItem menu>
-                    <a href='/servies'>Services</a>
-                </NavItem>
-                <NavItem menu>
-                    <a href='/contacts'>Contact Us</a>
-                </NavItem>
-            </ListContainer>
-        </Menu>
-      );
-
-return (
-    <ListContainer>
-        <NavItem>
-            <a style={{'textDecoration': 'none'}} href='#'>Home</a>
-        </NavItem>
-        <NavItem>
+      <Menu right styles={menuStyles}>
+        <ListContainer>
+          <NavItem menu>
+            <a href='#'>Home</a>
+          </NavItem>
+          <NavItem menu>
             <a href='/cars'>Cars</a>
-        </NavItem>
-        <NavItem>
+          </NavItem>
+          <NavItem menu>
             <a href='/servies'>Services</a>
-        </NavItem>
-        <NavItem>
+          </NavItem>
+          <NavItem menu>
             <a href='/contacts'>Contact Us</a>
-        </NavItem>
+          </NavItem>
+        </ListContainer>
+      </Menu>
+    );
+
+  return (
+    <ListContainer>
+      <NavItem>
+        <a style={{'textDecoration': 'none'}} href='#'>Home</a>
+      </NavItem>
+      <NavItem>
+        <a href='/cars'>Cars</a>
+      </NavItem>
+      <NavItem>
+        <a href='/servies'>Services</a>
+      </NavItem>
+      <NavItem>
+        <a href='/contacts'>Contact Us</a>
+      </NavItem>
     </ListContainer>
   )
 }
